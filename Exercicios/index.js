45c27655e803adba756862f893607e8c7439fd84
GUTEMBERG ALVES AMORIM <GUTEMBERG0150@DIGITALCOLLEGE.LOCAL>
2023-09-25 21:39:08 -0300

    s

--- Exercicios/index.js
+++ Exercicios/index.js
@@ -9,3 +9,28 @@ btnCalcular.addEventListener('click', () => {
     let sum = value1 * value2
     result.innerText = sum;
 });
+
+
+//Versão do Samuel
+
+// const input1 = document.getElementById("input1");
+// const input2 = document.getElementById("input2");
+// const btnCalc = document.getElementById("btnCalc");
+// const result = document.getElementById("resultado");
+
+// function sum(v1, v2) {
+//   return v1 * v2;
+// }
+
+// function showResult(resultado) {
+//   const finalResult = (result.innerText = resultado);
+//   return finalResult;
+// }
+
+// btnCalc.addEventListener("click", () => {
+//   let value1 = Number(input1.value);
+//   let value2 = Number(input2.value);
+//   const total = sum(value1, value2);
+//   return showResult(total);
+// });
+
